fe74964c2cdc801bc2ca4c4278dffa91d0921b1c
Hemanth <hemanth@kali.hemanth>
2024-03-06 00:03:58 +0530

    heyy assistant

---
+++ processor.py
@@ -0,0 +1,67 @@
+import nltk
+import tensorflow as tf
+from nltk.stem import WordNetLemmatizer
+import pickle
+import numpy as np
+
+nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('omw-1.4')
+
+model = tf.keras.models.load_model('chatbot_model.h5')
+import json
+import random
+intents = json.loads(open('job_intents.json', encoding='utf-8').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+
+lemmatizer = WordNetLemmatizer()
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+
+
+def bow(sentence, words, show_details=True):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+
+def predict_class(sentence, model):
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+        else:
+            result = "You must ask the right questions"
+    return result
+
+    
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
